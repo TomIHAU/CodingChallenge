7284420c4cf86a1c534baaf35ac2b78895b8c5e3
TauH <thaywood988@gmail.com>
2022-05-31 15:31:22 +1000

    working on the delete cascade

--- controllers/api/packagingRoute.js
+++ controllers/api/packagingRoute.js
@@ -1,14 +1,24 @@
 const router = require("express").Router();
-const { Packaging } = require("../../models");
+const { Packaging, Options } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
     const packagingData = await Packaging.findAll({});
-
     const packaging = await packagingData.map((Data) =>
       Data.get({ plain: true })
     );
+    res.status(200).json(packaging);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.post("/", async (req, res) => {
+  try {
+    const packagingData = await Packaging.findAll({});
+    const packaging = await packagingData.map((Data) =>
+      Data.get({ plain: true })
+    );
     res.status(200).json(packaging);
   } catch (err) {
     res.status(500).json(err);
