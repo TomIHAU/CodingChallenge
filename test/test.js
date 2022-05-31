d4d27d6e1ee3a3c0d3d9f4598804d8f6621679df
TauH <thaywood988@gmail.com>
2022-05-31 15:37:54 +1000

    added mocha need to add tests

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+const { ShoppingCart, apple, orange } = require("../");
+
+describe("", function () {
+  describe("#", function () {
+    it("", function () {});
+  });
+  describe("", function () {
+    it("", function () {
+      let cart = new ShoppingCart();
+      cart.addProduct = apple;
+      cart.addProduct = apple;
+      cart.addProduct = apple;
+      cart.removeProduct = apple;
+      assert.equal(cart.total, 9.9);
+    });
+  });
+});
