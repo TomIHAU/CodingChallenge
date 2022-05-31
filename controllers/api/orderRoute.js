ddcc546459ebe29a897bec67de5f1518730c9b5f
TauH <thaywood988@gmail.com>
2022-05-31 16:56:41 +1000

    order route logic work

--- controllers/api/orderRoute.js
+++ controllers/api/orderRoute.js
@@ -11,9 +11,33 @@ router.get("/", async (req, res) => {
   try {
     console.log(req.body, "helllo");
     let totalCost = 0;
+
+    //loops through the order by key
     for (let key in req.body) {
-      const ProductData = await Products.findOne({ where: { code: key } });
-      console.log(ProductData.get({ plain: true }), "oooooooooooo");
+      // if(!key){
+      //     res.status(400).json({message:"no su"})
+      // }
+      const productData = await Products.findOne({
+        where: { code: key },
+      });
+      if (!productData) {
+        res
+          .status(400)
+          .json({ message: "no such product please try ordering again" });
+        return;
+      }
+      const product = productData.get({ plain: true });
+
+      const packagingData = await Packaging.findOne({
+        where: { product_id: product.id },
+        include: Options,
+      });
+      if (packagingData) {
+        const packaging = packagingData.get({ plain: true });
+        console.log(packaging, "aaaaaaaaaaa");
+      }
+
+      console.log(product, "oooooooooooo");
     }
     const packaging = await packagingData.map((Data) =>
       Data.get({ plain: true })
