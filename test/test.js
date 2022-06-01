dab6ddc71216753d9e3cec317c7f62c1975339a4
TauH <thaywood988@gmail.com>
2022-06-01 10:23:56 +1000

    work on test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,31 @@
-var assert = require("assert");
+let assert = require("assert");
+const sequelize = require("../config/connection");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+const { doesNotMatch } = require("assert");
+let should = chai.should();
 
+chai.use(chaiHttp);
+
+describe("Testing Routes and logic", function () {
+  describe("#get products", function () {
+    it("get all products", function () {
+      chai
+        .request(server)
+        .get("/api/products")
+        .end((err, res) => {
+          console.log(res);
+          res.should.have.status(200);
+          res.body.should.be.a("array");
+          done();
+        });
+    });
+  });
+  // describe("", function () {
+  //   it("", function () {});
+  // });
+});
 // it("db.connection.connect should ...", function (done) {
 //   db.connection.connect(function (err, result) {
 //     if (err) {
@@ -11,13 +37,4 @@ var assert = require("assert");
 //   });
 // });
 
-describe("test", function () {
-  describe("#test", function () {
-    it("should test", function () {
-      let userRequest = { CE: 10, HM: 8 };
-    });
-  });
-  describe("", function () {
-    it("", function () {});
-  });
-});
+let userRequest = { CE: 10, HM: 8 };
