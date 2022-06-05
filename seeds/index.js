2793a4fb13183501b847ca32fd7bef1dd77a12d0
TauH <thaywood988@gmail.com>
2022-06-06 09:40:34 +1000

    removed unused seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,17 @@
 const sequelize = require("../config/connection");
 
 const seedProducts = require("./product-seeds");
-const seedPackaging = require("./packaging-seeds");
 const seedOptions = require("./options-seeds");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log("DATABASE SYNCED");
+  console.log("---------------DATABASE SYNCED---------------");
 
   await seedProducts();
-  console.log("PRODUCTS SYNCED");
-
-  await seedPackaging();
-  console.log("Packaging SYNCED");
+  console.log("---------------PRODUCTS SYNCED---------------");
 
   await seedOptions();
-  console.log("options SYNCED");
+  console.log("---------------options SYNCED---------------");
 
   process.exit(0);
 };
--- seeds/options-seeds.js
+++ seeds/options-seeds.js
@@ -2,27 +2,27 @@ const { Options } = require("../models");
 
 const OptionsData = [
   {
-    packaging_id: 1,
+    product_id: 1,
     quantity: 3,
     price: 1495,
   },
   {
-    packaging_id: 1,
+    product_id: 1,
     quantity: 5,
     price: 2095,
   },
   {
-    packaging_id: 2,
+    product_id: 2,
     quantity: 2,
     price: 1395,
   },
   {
-    packaging_id: 2,
+    product_id: 2,
     quantity: 5,
     price: 2995,
   },
   {
-    packaging_id: 2,
+    product_id: 2,
     quantity: 8,
     price: 4095,
   },
