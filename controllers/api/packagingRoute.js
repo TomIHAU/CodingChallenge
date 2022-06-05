f2f7cdbaab33336785f3e9ae94d01a169ea4ebe1
TauH <thaywood988@gmail.com>
2022-06-06 09:46:55 +1000

    changed packaging route

--- controllers/api/packagingRoute.js
+++ controllers/api/packagingRoute.js
@@ -1,24 +1,20 @@
 const router = require("express").Router();
-const { Packaging, Options, Products } = require("../../models");
+const { Options, Products } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const packagingData = await Packaging.findAll({
-      include: [{ model: Options }, { model: Products }],
+    const productData = await Products.findAll({
+      include: Options,
     });
-    const packaging = await packagingData.map((Data) =>
+    const packaging = await productData.map((Data) =>
       Data.get({ plain: true })
     );
     //returns the data in the structure requested in the assignment.
     res.status(200).json(
       packaging.map((e) => {
         return {
-          code: e.product.code,
-          options: [
-            ...e.options.map((e) => {
-              return { quantity: e.quantity, price: e.price };
-            }),
-          ],
+          code: e.code,
+          options: [...e.options],
         };
       })
     );
@@ -29,37 +25,24 @@ router.get("/", async (req, res) => {
 
 router.post("/:id", async (req, res) => {
   try {
-    let package;
+    const productData = await Products.findByPk(req.params.id);
 
-    const exists = await Packaging.findOne({
-      where: { product_id: req.params.id },
-    });
-
-    if (!exists) {
-      console.log("askdlfjhalskdjfhlaskjdfhlaskjdfhlaskjdfhlaskjdfhl");
-      const packagingData = await Packaging.Create({
-        product_id: req.params.id,
-      });
-      
-      console.log(packagingData);
-      package = packagingData.get({ plain: true });
-    } else {
-      package = exists.get({ plain: true });
+    if (!productData) {
+      res.status(400).json({ message: "the product id does not exist" });
     }
-    console.log("exists", exists ? true : false);
-    console.log("packagingData", package);
-    const { quantity, price } = req.body;
 
-    console.log("quantity, price", quantity, price);
+    const { quantity, price } = req.body;
 
     if (!quantity || !price) {
       res.status(400).json({ message: "please input new options data" });
     }
+
     const newOption = await Options.create({
-      packaging_id: package.id,
+      product_id: req.params.id,
       quantity,
       price,
     });
+
     res.status(200).json(newOption);
   } catch (err) {
     res.status(500).json(err);
